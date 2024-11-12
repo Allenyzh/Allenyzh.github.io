ac1ff1380ca858216b5d258bbc72f2e840ff9b73
Allenyzh <allenyangzhenhao@gmail.com>
2024-11-11 23:29:54 -0500

    -

--- script.js
+++ script.js
@@ -2,34 +2,47 @@ $(document).ready(function () {
   let currentYear = new Date().getFullYear();
   $("#spanYear").text(currentYear);
 
-  // 确保根据屏幕宽度设置导航状态
-  function adjustNav() {
-    if ($(window).width() > 700) {
-      $(".nav-list").show();
-    } else {
-      $(".nav-list").hide();
-    }
-  }
+  //   function adjustNav() {
+  //     if ($(window).width() > 700) {
+  //       $(".nav-list").show();
+  //     } else {
+  //       $(".nav-list").hide();
+  //     }
+  //   }
+  //   adjustNav();
 
-  // 调用函数以设置初始状态
-  adjustNav();
-
-  // 切换汉堡菜单
   $("#mobile-menu").click(function () {
     $(".nav-list").toggle();
   });
 
-  // 点击导航链接时隐藏导航列表
-  $(".nav-list a").click(function () {
-    if ($(".nav-list").is(":visible") && $(window).width() <= 700) {
+  $("#mobile-menu").click(function colorSwitch() {
+    if ($(".nav-list").is(":visible") && $(window).width() <= 800) {
+      $(".bar").css("background-color", "#242424");
+    } else {
+      $(".bar").css("background-color", "aliceblue");
+    }
+  });
+
+  $(window).on("wheel", function (event) {
+    if (event.originalEvent.deltaY !== 0 && $(window).width() <= 800) {
       $(".nav-list").hide();
+      $(".bar").css("background-color", "aliceblue");
+    }
+  });
+
+  // 点击导航链接时隐藏导航列表
+  $(".nav-list a").click(() => {
+    if ($(".nav-list").is(":visible") && $(window).width() <= 800) {
+      $(".nav-list").hide();
+      $(".bar").css("background-color", "aliceblue");
     }
   });
 
   // 监听窗口尺寸变化
   $(window).resize(function () {
-    if ($(window).width() > 700) {
+    if ($(window).width() > 800) {
       $(".nav-list").show();
+      $(".nav-list").css("display", "flex");
     } else {
       $(".nav-list").hide();
     }
@@ -42,18 +55,17 @@ $(document).ready(function () {
 let myArray = ["Full Stack Developer", "Passionate Learner", "Math Student"];
 function textReplace() {
   let arrayIndex = 0;
-  const typingElement = $("#typingText"); // 使用 jQuery 选择器
 
   setInterval(function () {
     // 开始淡出
-    typingElement.fadeOut(1000, function () {
+    $("#typingText").fadeOut(1000, function () {
       // 淡出后更换文本
-      typingElement.text(myArray[arrayIndex]);
+      $("#typingText").text(myArray[arrayIndex]);
       // 更新索引
       arrayIndex = (arrayIndex + 1) % myArray.length;
       //   console.log(arrayIndex);
       // 开始淡入
-      typingElement.fadeIn(1000);
+      $("#typingText").fadeIn(1000);
     });
   }, 3000);
 }

2cd282ea169f874487b0147a5958554477f8a9da
Allenyzh <allenyangzhenhao@gmail.com>
2024-11-12 20:19:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -17,8 +17,12 @@ $(document).ready(function () {
 
   // 手机滚动事件监听
   $(window).scroll(function (event) {
-    if (event.originalEvent.deltaY !== 0 && $(window).width() <= 800 && $(".nav-list").is(":visible")) {
-      console.log('scroll1');
+    if (
+      event.originalEvent.deltaY !== 0 &&
+      $(window).width() <= 800 &&
+      $(".nav-list").is(":visible")
+    ) {
+      console.log("scroll1");
       barColorWhite();
     }
   });
@@ -32,15 +36,13 @@ $(document).ready(function () {
 
   // 监听全页面的点击事件
   $(document).on("click", function (event) {
-    // 检查点击是否发生在 nav 或其内部，以及 mobile-menu 上
     if (!$(event.target).closest("nav, #mobile-menu").length) {
-      barColorWhite(); // 如果点击的不是 nav 或 mobile-menu，则关闭菜单
+      barColorWhite();
     }
   });
 
-  // 如果你还需要绑定点击 nav 但不关闭菜单的事件，可以添加此代码
   $("nav").on("click", function (event) {
-    event.stopPropagation(); // 阻止 nav 内的点击事件冒泡到 document
+    event.stopPropagation();
   });
 
   // 监听窗口尺寸变化
@@ -48,10 +50,8 @@ $(document).ready(function () {
     if ($(window).width() > 800) {
       $(".nav-list").show();
       $(".nav-list").css("display", "flex");
-      // console.log($(".nav-list").is(":visible"));
     } else {
       $(".nav-list").hide();
-      // console.log($(".nav-list").is(":visible"));
     }
   });
 
@@ -62,18 +62,15 @@ $(document).ready(function () {
 });
 
 function debounce(func, delay) {
-  let timeout; // 用于存储setTimeout返回的ID
+  let timeout;
 
-  // 返回一个新函数，这个新函数会延迟执行传入的func
-  return function() {
-    const context = this; // 保留调用上下文
-    const args = arguments; // 保留传递给函数的参数
+  return function () {
+    const context = this;
+    const args = arguments;
 
-    // 清除之前的延时器
     clearTimeout(timeout);
 
-    // 设置新的延时器
-    timeout = setTimeout(function() {
+    timeout = setTimeout(function () {
       func.apply(context, args);
     }, delay);
   };
@@ -81,7 +78,6 @@ function debounce(func, delay) {
 
 function toggleMenuColor() {
   if (!$(".nav-list").is(":visible") && $(window).width() <= 800)
-    // console.log(!$(".nav-list").is(":visible"));
     $(".bar").css("background-color", "aliceblue");
 }
 
@@ -95,14 +91,11 @@ function textReplace() {
   let arrayIndex = 0;
 
   setInterval(function () {
-    // 开始淡出
     $("#typingText").fadeOut(1000, function () {
-      // 淡出后更换文本
       $("#typingText").text(myArray[arrayIndex]);
-      // 更新索引
+
       arrayIndex = (arrayIndex + 1) % myArray.length;
-      //   console.log(arrayIndex);
-      // 开始淡入
+
       $("#typingText").fadeIn(1000);
     });
   }, 3000);
